ac502033d342bda6c1ea2a14508c4eb791ed1557
udacityKN <40337686+udacityKN@users.noreply.github.com>
2018-07-14 16:34:04 -0500

    Create a Map Component

---
+++ src/MapComponent.js
@@ -0,0 +1,19 @@
+/*
+Neighborhood Map Project - Map Component
+*/
+
+
+import React, { Component } from 'react';
+import './App.css';
+
+class MapComponent extends Component {
+  render() {
+    return (
+      <div className="MapComponent">
+		
+      </div>
+    );
+  }
+}
+
+export default MapComponent;
